0a41d980b9968e373b8619281bc02b91ae303ad4
Optional Dev <78622778+optionaldev@users.noreply.github.com>
2026-04-30 18:29:06 +0300

    Added Xcode project

---
+++ Workspace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workspace
+//
+//  Created by Alexandru Pavalache on 30.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workspace/WorkspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkspaceApp.swift
+//  Workspace
+//
+//  Created by Alexandru Pavalache on 30.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct WorkspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
