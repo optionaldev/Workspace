e09a5883fffd98c22bd9d28e19dae43d1b7f7d57
Optional Dev <78622778+optionaldev@users.noreply.github.com>
2026-05-21 21:50:13 +0300

    Added mock for shopping list.

---
+++ Workspace/Shopping/ShoppingMock.swift
@@ -0,0 +1,52 @@
+//
+//  ShoppingMock.swift
+//  Workspace
+//
+//  Created by Alexandru Pavalache on 21.05.2026.
+//
+
+
+
+extension Constants {
+    static let shoppingMock = """
+[
+    {
+        "en": "eggs",
+        "es": "huevos",
+        "ro": "ouǎ"
+    },{
+        "en": "vegetables",
+        "es": "verduras",
+        "ro": "legume"
+    },{
+        "en": "fruits",
+        "es": "frutas",
+        "ro": "fructe"
+    },{
+        "en": "yogur",
+        "es": "yogur",
+        "ro": "iaurt"
+    },{
+        "en": "salmon",
+        "es": "salmón",
+        "ro": "somon"
+    },{
+        "en": "sardines",
+        "es": "sardinas",
+        "ro": "sardine"
+    },{
+        "en": "sparkling water",
+        "es": "agua con gas",
+        "ro": "apa carbogazificata"
+    },{
+        "en": "milk",
+        "es": "leche",
+        "ro": "lapte"
+    },{
+        "en": "snacks",
+        "es": "pica-pica",
+        "ro": "snacksuri"
+    }
+]
+"""
+}
