0012696e75b7b5584eec5d41e81589942d729ecc
Optional Dev <78622778+optionaldev@users.noreply.github.com>
2026-06-05 22:31:30 +0300

    Added notification permission request

--- Workspace/Reminders/ReminderView.swift
+++ Workspace/Reminders/ReminderView.swift
@@ -12,10 +12,22 @@ struct ReminderView: View {
     @StateObject private var viewModel = ReminderViewModel()
     
     var body: some View {
-        List(viewModel.reminders, id: \.id) { item in
-            Text(item.id)
+        switch viewModel.authorizationStatus {
+        case .authorized:
+            List(viewModel.reminders, id: \.id) { item in
+                Text(item.id)
+            }
+            .foregroundStyle(.brown)
+        case .notDetermined:
+            Text("Allow notifications in order to remind you at your chosen time.")
+                .multilineTextAlignment(.center)
+                .padding(15)
+            Button("Request authorization") {
+                viewModel.requestNotificationPermission()
+            }
+        default:
+            Text("Something went wrong.")
         }
-        .foregroundStyle(.brown)
     }
 }
 
--- Workspace/Reminders/ReminderViewModel.swift
+++ Workspace/Reminders/ReminderViewModel.swift
@@ -5,12 +5,40 @@
 //  Created by Alexandru Pavalache on 04.05.2026.
 //
 
-import Foundation
 import Combine
+import Foundation
+import UserNotifications
+import UIKit
+
+enum NotificationAuthorizationStatus {
+    
+    case notKnown
+    case granted
+    case denied
+}
 
 final class ReminderViewModel: ObservableObject {
     
+    @Published var authorizationStatus: UNAuthorizationStatus = .notDetermined
+    
     @Published var reminders: [Reminder]
+    
+    func requestNotificationPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, error in
+            if let error {
+                debug(error)
+                return
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                if granted {
+                    self?.authorizationStatus = .authorized
+                } else {
+                    self?.authorizationStatus = .denied
+                }
+            }
+        }
+    }
     
     init() {
         if let reminderData = Constants.reminderMock.data(using: .utf8) {
@@ -18,12 +46,18 @@ final class ReminderViewModel: ObservableObject {
                 let reminderDecodables = try JSONDecoder().decode([ReminderDecodable].self, from: reminderData)
                 reminders = reminderDecodables.compactMap { Reminder(fromDecodable: $0) }
                 debug("reminders read successfully \(reminders.count)")
-                return
             } catch {
                 debug(error)
+                reminders = []
+            }
+        } else {
+            reminders = []
+        }
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            debug("getNotificationSettings returned \(settings)")
+            DispatchQueue.main.async { [weak self] in
+                self?.authorizationStatus = settings.authorizationStatus
             }
         }
-        debug("reminders is empty")
-        reminders = []
     }
 }
